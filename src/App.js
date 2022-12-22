6ac6e3eea72041f534a1a2a0680db98cf79705ad
N7954 <duderedacted@gmail.com>
2022-12-22 22:29:57 +0300

    düzelttim

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout';
-import Home from './components/Home';
+//import Home from './components/Home';
 
 function App() {
   return (
